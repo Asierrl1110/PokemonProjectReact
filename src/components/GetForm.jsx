80fabe88ecd6ce5c816b151129d211b7467bd70b
Asierrl <asierlamas05@gmail.com>
2025-07-04 16:19:27 +0200

    Proyecto práctico: Creando información mediante formularios

---
+++ src/components/GetForm.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+function GetForm(props) {
+    const [from, setFrom] = useState(1);
+    const [to, setTo] = useState(10);
+
+    const handleFromInput = (e) => {
+        setFrom(e.target.value);
+    }
+
+    const handleToInput = (e) => {
+        setTo(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        props.getPokemons(from,to);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <label htmlFor="from-pokemon">From:</label>
+                <input type="number" id="from-pokemon" onChange={handleFromInput} min={1} />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="to-pokemon">To:</label>
+                <input type="number" id="to-pokemon" onChange={handleToInput} min={1} />
+            </fieldset>
+            <button>Get Pokemon!</button>
+        </form>
+  )
+}
+
+export default GetForm
