33e2094b925c569cb301e7b782339855d6cb2591
Asierrl <asierlamas05@gmail.com>
2025-07-14 12:04:45 +0200

    Creación de un contexto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PokemonProviderWrapper } from './context/pokemon.context.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+    <PokemonProviderWrapper>
+      <App />
+    </PokemonProviderWrapper>
   // </StrictMode>
 )
