980f3af2b89eff1ecc4d647347922a18488935a3
Asierrl <asierlamas05@gmail.com>
2025-07-15 11:31:13 +0200

    Curso intermedio: Enrutamiento y Navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,21 @@
-import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import PokemonList from './components/PokemonList'
-import PokemonDetails from './components/PokemonDetails';
+import HomePage from './pages/HomePage'
+import PokemonsPage from './pages/PokemonsPage'
+import PokemonPage from './pages/PokemonPage'
+import ErrorPage from './pages/ErrorPage'
 import PokemonDetails2 from './components/PokemonDetails2';
 import DetailsWrapper from './hoc/DetailsWrapper';
 
 function App() {
-  // Estado para almacenar el Pokémon seleccionado desde la lista
-  const [selectedPokemon, setSelectedPokemon] = useState();
-  // Estado para almacenar el Pokémon seleccionado 2 desde la lista
-  const [selectedPokemon2, setSelectedPokemon2] = useState();
-
-  const getDetails1 = (likes, increaseLikes) => {
-    return (
-      <PokemonDetails 
-      pokemon={selectedPokemon}
-      likes={likes}
-      increaseLikes={increaseLikes}
-      ></PokemonDetails>
-    )
-  }
-
-  const getDetails2 = (likes, increaseLikes) => {
-    return (
-      <PokemonDetails2 
-      pokemon={selectedPokemon}
-      likes={likes}
-      increaseLikes={increaseLikes}
-      ></PokemonDetails2>
-    )
-  }
-
   return (
-    <>
-      {/* Si hay un Pokémon seleccionado, se muestra la sección de detalles */}
-      <h2>Pokemons Seleccionado</h2>
-      {/* Componente que muestra los detalles del Pokémon seleccionado */}
-      {selectedPokemon && (
-          <DetailsWrapper render={getDetails1}></DetailsWrapper>
-      )}
-      {/* Componente que muestra los detalles del Pokémon seleccionado */}
-      {selectedPokemon2 && (
-          <DetailsWrapper render={getDetails2}></DetailsWrapper>
-      )}
-      <h2>Lista de Pokemons</h2>
+    <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+      <Route path="/pokemons" element={<PokemonsPage/>}></Route>
+      <Route path="/pokemon/:id" element={<PokemonPage/>}></Route>
 
-      {/* Componente que muestra la lista de Pokémons.
-          Se le pasa una función para seleccionar un Pokémon */}
-      <PokemonList selectPokemon={setSelectedPokemon}
-                   selectPokemon2={setSelectedPokemon2}></PokemonList> 
-    </>
+      <Route path="*" element={<ErrorPage/>}></Route>
+    </Routes>
   )
 }
 
